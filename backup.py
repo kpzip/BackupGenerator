3517aa5f6f20494f3ac0eac5fb623f0b9c3003df
kpzip <kpzipgames@gmail.com>
2024-02-05 10:32:10 -0800

    refactor

--- backup.py
+++ backup.py
@@ -3,31 +3,55 @@ import pysftp
 import os
 from stat import S_ISDIR, S_ISREG
 
-#Thanks, stackoverflow
-def get_r_portable(sftp, remotedir, localdir, preserve_mtime=False):
-    for entry in sftp.listdir_attr(remotedir):
-        remotepath = remotedir + "/" + entry.filename
-        localpath = os.path.join(localdir, entry.filename)
-        mode = entry.st_mode
-        if S_ISDIR(mode):
-            try:
-                os.mkdir(localpath)
-            except OSError:     
-                pass
-            get_r_portable(sftp, remotepath, localpath, preserve_mtime)
-        elif S_ISREG(mode):
-            sftp.get(remotepath, localpath, preserve_mtime=preserve_mtime)
+#TODO, improve this by only reading a certain number of bytes at a time
+class FileSystemInterface:   
+    
+    def write(self, filepath: str, filedata: bytearray) -> None:
+        pass
+    
+    def read(self, filepath: str) -> bytearray:
+        pass
+        
+
+class SFTPFileSystem(FileSystemInterface):
+    
+    def __init__(self, sftpaddr: str, user: str, password: str):
+        self.connection = pysftp.Connection(sftpaddr, Username = user, Password = password)
+    
+    def write(self, filepath: str, filedata: bytearray) -> None:
+        pass
+    
+    def read(self, filepath: str) -> bytearray:
+        pass
+
+class LocalFileSystem(FileSystemInterface):
+    
+    def __init__(self):
+        pass
+    
+    def write(self, filepath: str, filedata: bytearray) -> None:
+        pass
+    
+    def read(self, filepath: str) -> bytearray:
+        pass
+
+def file_system_factory(fstype: str, config: dict) -> FileSystemInterface:
+    if (fstype == "sftp"):
+        return SFTPFileSystem(config["sftp_addr"], config["sftp_user"], config["sftp_pass"])
+    if (fstype == "local"):
+        return LocalFileSystem()
+    return None
 
 def main():
     print("Initiating Backup...")
     cfgfile = open("config.json")
     cfg = json.load(cfg)
     cfgfile.close()
-    if cfg["from"] == "sftp" and cfg["to"] == "local":
-        sftp = pysftp.Connection(cfg["sftp_addr"], Username = cfg["sftp_user"], Password = cfg["sftp_pass"])
-        for filedata in cfg["files"]:
-            get_r_portable(sftp, filedata[0], filedata[1], preserve_mtime = True)
-        sftp.close()
+    fs_from = file_system_factory(cfg["from"], cfg)
+    fs_to = file_system_factory(cfg["to"], cfg)
+    for file in cfg["files"]:
+        to.write(file, from.read(file))
+    print("Backup Complete!")
 
 if __name__ == "__main__":
     main()

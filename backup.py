4e8f89b094041562db97993de6fc66c46728084a
kpzip <kpzipgames@gmail.com>
2024-02-05 21:34:16 -0800

    fix typing and use with statements

--- backup.py
+++ backup.py
@@ -1,14 +1,18 @@
 import json
 import pysftp as sftp
 import os
+import getpass
+
+#Modify if you want the config file named something else, or in another directory
+config_name = "config.json"
 
 #TODO, improve this by only reading a certain number of bytes at a time
 class FileSystemInterface:   
     
-    def writeFile(self, filepath: str, filedata: str) -> None:
+    def writeFile(self, filepath: str, filedata: bytes) -> None:
         pass
     
-    def readFile(self, filepath: str) -> str:
+    def readFile(self, filepath: str) -> bytes:
         pass
     
     def getFilesRecursive(self, path: str) -> list:
@@ -19,10 +23,10 @@ class SFTPFileSystem(FileSystemInterface):
     def __init__(self, sftpaddr: str, user: str, password: str):
         self.connection = sftp.Connection(sftpaddr, Username = user, Password = password)
     
-    def writeFile(self, filepath: str, filedata: str) -> None:
+    def writeFile(self, filepath: str, filedata: bytes) -> None:
         pass
     
-    def readFile(self, filepath: str) -> str:
+    def readFile(self, filepath: str) -> bytes:
         pass
         
     def getFilesRecursive(self, path: str) -> list:
@@ -33,12 +37,12 @@ class LocalFileSystem(FileSystemInterface):
     def __init__(self):
         pass
     
-    def writeFile(self, filepath: str, filedata: str) -> None:
+    def writeFile(self, filepath: str, filedata: bytes) -> None:
         os.makedirs(os.path.dirname(filepath), exist_ok = True)
         with open(filepath, "wb") as file:
             file.write(filedata)
     
-    def readFile(self, filepath: str) -> str:
+    def readFile(self, filepath: str) -> bytes:
         with open(filepath, "rb") as file:
             return file.read()
     
@@ -52,6 +56,13 @@ class LocalFileSystem(FileSystemInterface):
 
 def file_system_factory(fstype: str, config: dict) -> FileSystemInterface:
     if (fstype == "sftp"):
+        addr = config["sftp_addr"]
+        user = config["sftp_user"]
+        passwd = config["sftp_pass"]
+        if user == "prompt":
+            user = input("SFTP username for address " + addr + " :")
+        if passwd == "prompt":
+            passwd = getpass.getpass(prompt=("SFTP password for address " + addr + " :"))
         return SFTPFileSystem(config["sftp_addr"], config["sftp_user"], config["sftp_pass"])
     if (fstype == "local"):
         return LocalFileSystem()
@@ -62,9 +73,8 @@ def copy_file(fs_in: FileSystemInterface, fs_out: FileSystemInterface, dir_in: s
 
 def main(configLocation: str) -> None:
     print("Initiating Backup...")
-    cfgfile = open(configLocation)
-    cfg = json.load(cfgfile)
-    cfgfile.close()
+    with open(configLocation) as cfgfile:
+        cfg = json.load(cfgfile)
     fs_from = file_system_factory(cfg["from"], cfg)
     fs_to = file_system_factory(cfg["to"], cfg)
     for file in cfg["files"]:
@@ -75,4 +85,4 @@ def main(configLocation: str) -> None:
     print("Backup Complete!")
 
 if __name__ == "__main__":
-    main("config.json")
+    main(config_name)
